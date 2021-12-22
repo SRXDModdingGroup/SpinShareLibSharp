f5e30a1267da46cd7190b9f1a74ac0fe1c43a619
Amy Y <ctrlalternativedelete@gmail.com>
2021-12-22 16:47:11 +1100

    init

---
+++ SpinShareLib/SpinShareLib.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using SpinShareLib.Types;
+
+namespace SpinShareLib
+{
+    public class SSAPI
+    {
+        public string apiBase { get; private set; }
+        public int supportedVersion { get; private set; }
+
+        public HttpClient client { get; private set; }
+
+        public SSAPI()
+        {
+            this.apiBase = "https://spinsha.re/api/";
+            this.supportedVersion = 1;
+            this.client = new HttpClient();
+        }
+        
+        async public Task<Content> ping()
+        {
+            return await this.getApiResultAsType<Content>($"{this.apiBase}ping");
+        }
+
+        async public Task<Content<Promo[]>> getPromos()
+        {
+            return await this.getApiResultAsType<Content<Promo[]>>($"{this.apiBase}promos");
+        }
+
+        async public Task<Content<Song[]>> getNewSongs(int _offset)
+        {
+            return await this.getApiResultAsType<Content<Song[]>>($"{this.apiBase}songs/new/{_offset}");
+        }
+        async public Task<Content<Song[]>> getHotThisWeekSongs(int _offset)
+        {
+            return await this.getApiResultAsType<Content<Song[]>>($"{this.apiBase}songs/hotThisWeek/{_offset}");
+        }
+
+        async public Task<Content<SongDetail>> getSongDetail(string _songId)
+        {
+            return await this.getApiResultAsType<Content<SongDetail>>($"{this.apiBase}song/{_songId}");
+        }
+
+        async public Task<Content<Reviews>> getSongDetailReviews(string _songId)
+        {
+            return await this.getApiResultAsType<Content<Reviews>>($"{this.apiBase}song/{_songId}/reviews");
+        }
+
+        async public Task<Content<SpinPlays>> getSongDetailSpinPlays(string _songId)
+        {
+            return await this.getApiResultAsType<Content<SpinPlays>>($"{this.apiBase}song/{_songId}/spinplays");
+        }
+        async public Task<Content<UserDetail>> getUserDetail(string _userId)
+        {
+            return await this.getApiResultAsType<Content<UserDetail>>($"{this.apiBase}user/{_userId}");
+        }
+        async public Task<Content<Song[]>> getUserCharts(string _userId)
+        {
+            return await this.getApiResultAsType<Content<Song[]>>($"{this.apiBase}user/{_userId}/charts");
+        }
+        async public Task<Content<Reviews.Review[]>> getUserReviews(string _userId)
+        {
+            return await this.getApiResultAsType<Content<Reviews.Review[]>>($"{this.apiBase}user/{_userId}/reviews");
+        }
+        async public Task<Content<SpinPlays.Spinplay[]>> getUserSpinPlays(string _userId)
+        {
+            return await this.getApiResultAsType<Content<SpinPlays.Spinplay[]>>($"{this.apiBase}user/{_userId}/spinplays");
+        }
+
+        async public Task<Content<Search>> search(string _query)
+        {
+            return await this.getApiResultAsType<Content<Search>>($"{this.apiBase}search/{_query}");
+        }
+
+        async private Task<T> getApiResultAsType<T>(string apiPath)
+        {
+            var resp = await client.GetAsync(apiPath);
+            if (resp.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<T>(await resp.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+    }
+
+    namespace Types
+    {
+        public class Content
+        {
+            public int version;
+            public int status;
+        }
+
+        public class Content<T> : Content
+        {
+            public T data;
+        }
+
+        public class Promo
+        {
+            public int id;
+            public string
+                title,
+                type,
+                textColor,
+                color,
+                image_path;
+            public bool isVisible;
+            public Button button;
+            public class Button
+            {
+                public int type;
+                public string data;
+            }
+        }
+
+        public class Song
+        {
+            public int? 
+                id,
+                easyDifficulty,
+                normalDifficulty,
+                hardDifficulty,
+                expertDifficulty,
+                XDDifficulty;
+            public bool
+                hasEasyDifficulty,
+                hasNormalDifficulty,
+                hasHardDifficulty,
+                hasExpertDifficulty,
+                hasXDDifficulty;
+            public string 
+                title,
+                subtitle,
+                artist,
+                Songer,
+                updateHash,
+                cover,
+                zip;
+        }
+        public class SongDetail : Song
+        {
+            public int?
+                uploader,
+                views,
+                downloads,
+                publicationStatus;
+            public string description;
+            public string[] tags;
+            public Date uploadDate;
+            public Paths paths;
+            public class Paths
+            {
+                public string
+                    ogg,
+                    cover,
+                    zip;
+            }
+        }
+        public class Reviews
+        {
+            public bool average;
+            public Review[] reviews;
+            public class Review
+            {
+                public int id;
+                public SongDetail song;
+                public User user;
+                public bool recommended;
+                public string comment;
+                public Date reviewDate;
+            }
+        }
+        public class SpinPlays
+        {
+            public Spinplay[] spinPlays;
+            public class Spinplay
+            {
+                public int id;
+                public User user;
+                public bool? isActive;
+                public Date submitDate;
+                public string
+                    videoUrl,
+                    videoThumbnail;
+
+            }
+        }
+        public class User
+        {
+            public int id;
+            public bool?
+                isVerified,
+                isPatreon;
+            public string
+                username,
+                avatar;
+        }
+        public class UserDetail : User
+        {
+            public string pronouns;
+            public int?
+                songs,
+                playlists,
+                reviews,
+                spinplay;
+            public Card[] cards;
+            public class Card
+            {
+                public int id;
+                public string
+                    icon,
+                    title,
+                    description;
+                public Date givenDate;
+                public int?
+                    songs,
+                    playlists,
+                    reviews,
+                    spinplay;
+            }
+        }
+
+        public class Search
+        {
+            public Song[] songs;
+            public User[] users;
+        }
+
+        public class Date
+        {
+            public string 
+                date,
+                timezone;
+            public int timezone_type;
+        }
+    }
+}
---
+++ SpinShareLibTests/SSAPITests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpinShareLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SpinShareLib.Tests
+{
+    [TestClass()]
+    public class SSAPITests
+    {
+        public SSAPI inst;
+        public SSAPITests()
+        {
+            inst = new SSAPI();
+        }
+
+        [TestMethod()]
+        public void ping()
+        {
+            Task.Run(async () => {
+               var thing = await inst.ping();
+               Console.WriteLine(thing.status);
+            }).GetAwaiter().GetResult();
+            
+        }
+        [TestMethod()]
+        public void getPromos()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getPromos();
+                Console.WriteLine(thing.data[0].button.data);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getNewSongs()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getNewSongs(0);
+                Console.WriteLine(thing.data[0].id);
+            }).GetAwaiter().GetResult();
+
+        }
+        [TestMethod()]
+        public void getHotThisWeekSongs()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getHotThisWeekSongs(0);
+                Console.WriteLine(thing.data[0].id);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getSongDetail()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getSongDetail("1234");
+                Console.WriteLine(thing.data.title);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getSongDetailReviews()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getSongDetailReviews("1234");
+                Console.WriteLine(thing.data.reviews[0].comment);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getSongDetailSpinPlays()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getSongDetailSpinPlays("1234");
+                Console.WriteLine(thing.data.spinPlays[0].videoUrl);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getUserDetail()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getUserDetail("72");
+                Console.WriteLine(thing.data.username);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getUserCharts()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getUserCharts("72");
+                Console.WriteLine(thing.data[0].title);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getUserReviews()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getUserReviews("72");
+                Console.WriteLine(thing.data[0].id);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void getUserSpinPlays()
+        {
+            Task.Run(async () => {
+                var thing = await inst.getUserSpinPlays("20");
+                Console.WriteLine(thing.data[0].id);
+            }).GetAwaiter().GetResult();
+        }
+        [TestMethod()]
+        public void search()
+        {
+            Task.Run(async () => {
+                var thing = await inst.search("ayanamy");
+                Console.WriteLine(thing.data.users[0].id);
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
