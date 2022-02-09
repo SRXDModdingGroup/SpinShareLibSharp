35db3c4dc666892293e0c078b57784c7a9e10ed5
amy <ctrlalternativedelete@gmail.com>
2022-02-09 21:01:42 +1100

    added simple program for testing

---
+++ SimpleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SpinShareLib;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ssapi = new SSAPI();
+            Task.Run(async () => {
+                var thing = await ssapi.ping();
+                Console.WriteLine(thing.status);
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
